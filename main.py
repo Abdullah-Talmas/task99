6e1ff190ff684f3bc0a18227ebeace35c01598c5
Abdullah-Talmas <92312179+Abdullah-Talmas@users.noreply.github.com>
2023-06-18 15:48:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from Rectangle import Rectangle, Parallelepipede
+
+my_rectangle = Rectangle(5, 3)
+my_rectangle.display()
+
+my_parallelepipede = Parallelepipede(4, 6, 2)
+my_parallelepipede.display()
+
+print(f"Volume: {my_parallelepipede.volume()}")
+
+
+
+
+
